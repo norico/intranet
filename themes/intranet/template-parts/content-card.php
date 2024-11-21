50400dac424f04b48c259706209030482bd37930
norico <legion.norico@gmail.com>
2024-11-21 23:42:21 +0100

    flex card

--- themes/intranet/template-parts/content-card.php
+++ themes/intranet/template-parts/content-card.php
@@ -1,11 +1,36 @@
-<div class="card border border slate-500">
-    <div class="card-thumbnail">
-        <?php the_post_thumbnail('', array('class' => 'aspect-video object-cover')); ?>
+<div id="card" class="flex flex-col border border slate-500 shadow-md rounded-xl">
+    <div id="card-thumbnail" class="hover:shadow-md rounded-t-xl">
+        <div class="overflow-hidden">
+            <a class="" href="<?php the_permalink();?>">
+            <?php the_post_thumbnail('', array('class' => 'aspect-video object-cover hover:scale-110 duration-700')); ?>
+            </a>
+        </div>
     </div>
-    <div class="m-2">
-        <div class="card-title"><?php the_title('<h1 class="text-2xl">','</h1>') ?></div>
-        <div class="card-site">From: <?php bloginfo("name"); ?></div>
-        <div class="card-content"><?php the_excerpt(); ?></div>
-    </div>
+    <div id="card-content" class="flex flex-col gap-1 h-full mx-2">
 
+        <div id="card-post-title" class="min-h-24">
+            <a class="hover:underline" href="<?php the_permalink();?>">
+		    <?php the_title('<h1 class="text-2xl text-balance line-clamp-3">','</h1>') ?></div>
+        </a>
+
+        <div id="card-post-site" class="">
+            <a class="hover:underline" href="<?php bloginfo("url"); ?>">
+			    <?php bloginfo("name"); ?>
+            </a>
+        </div>
+
+        <div id="card-post-content" class="grow">
+            <div class="line-clamp-6"><?php the_excerpt(); ?></div>
+        </div>
+        <div id="card-post-footer" class="my-2">
+            <span class="mr-1 inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">
+                <?php the_category('</span><span class="mr-1 inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">'); ?>
+            </span>
+            <?php if ( has_tag()) : ?>
+            <span class="mr-1 inline-flex items-center rounded-md bg-red-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-red-500/10">
+                <?php the_tags('','</span><span class="mr-1 inline-flex items-center rounded-md bg-red-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-red-500/10">',''); ?>
+            </span>
+            <?php endif; ?>
+        </div>
+    </div>
 </div>
